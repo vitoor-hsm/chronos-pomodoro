72b4eb24fb1e4de82d9377f5582a1ffde61889e3
Victor <vhgo2851@gmail.com>
2026-03-07 22:23:25 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import './styles/global.css';
+import './styles/theme.css';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+
+export function App() {
+    return (
+        <>
+            <Container>
+                <Logo />
+            </Container>
+
+            <Container>
+                <Menu />
+            </Container>
+
+            <Container>
+                <CountDown />
+            </Container>
+
+            <Container>
+                <form className='form' action="">
+                    <div className='formRow'>
+                        <label htmlFor="input">task</label>
+                        <input id='input' type="text" />
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0 0</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <button>Enviar</button>
+                    </div>
+
+                </form>
+            </Container>
+        </>
+    );
+}
