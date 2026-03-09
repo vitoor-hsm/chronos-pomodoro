5ee14f9e5b7e0e5b68fdb0b385d4a3a42e4805e0
Victor <vhgo2851@gmail.com>
2026-03-09 16:28:44 -0300

    cria componente DefaultInput e atualiza App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
     return (
@@ -23,8 +24,7 @@ export function App() {
             <Container>
                 <form className='form' action="">
                     <div className='formRow'>
-                        <label htmlFor="input">task</label>
-                        <input id='input' type="text" />
+                        <DefaultInput id='meuInput' type='text' />
                     </div>
 
                     <div className='formRow'>
