4509421709b055fd0af609b0f91f2c91b81c28e7
Victor <vhgo2851@gmail.com>
2026-03-11 10:54:30 -0300

    atualizando o input

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ export function App() {
             <Container>
                 <form className='form' action="">
                     <div className='formRow'>
-                        <DefaultInput id='meuInput' type='text' />
+                        <DefaultInput labelText='' id='meuInput' type='text' />
                     </div>
 
                     <div className='formRow'>
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,11 +1,13 @@
 type DefaultInputProps = {
     id: string
+    labelText?: string
 } & React.ComponentProps<'input'>;
-export function DefaultInput({ id, type }: DefaultInputProps) {
+export function DefaultInput({ id, type, labelText, ...rest }: DefaultInputProps) {
     return (
         <>
-        <label htmlFor="id">task</label>
-        <input id={id} type={type} />
+        {labelText && <label htmlFor="id">{labelText} </label>}
+        <input id={id} type={type} {...rest} />
         </>
     );
 }
+
