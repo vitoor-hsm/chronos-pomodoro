9543c0a0d4e5e27872a0b01a41bf1e06e5146575
Victor <vhgo2851@gmail.com>
2026-03-12 15:05:02 -0300

    estilos dos ciclos

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 export function App() {
     return (
@@ -32,8 +33,7 @@ export function App() {
                     </div>
 
                     <div className='formRow'>
-                        <p>Ciclos</p>
-                        <p>0 0 0 0 0 0</p>
+                        <Cycles/>
                     </div>
 
                     <div className='formRow'>
