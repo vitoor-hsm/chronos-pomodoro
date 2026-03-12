7f888bf02ad370a228e124a873a479284b04acc6
Victor <vhgo2851@gmail.com>
2026-03-12 19:45:33 -0300

    inciando botão de play/stop

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
 
 export function App() {
     return (
@@ -37,7 +39,7 @@ export function App() {
                     </div>
 
                     <div className='formRow'>
-                        <button>Enviar</button>
+                        <DefaultButton icon={<PlayCircleIcon/> } />
                     </div>
 
                 </form>
