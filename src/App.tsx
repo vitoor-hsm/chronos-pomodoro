1ecea9b7048396c8a33935907831bcb5ca55c8ad
Victor <vhgo2851@gmail.com>
2026-03-13 18:48:10 -0300

    footer criado

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
     return (
@@ -44,6 +45,11 @@ export function App() {
 
                 </form>
             </Container>
+
+            <Container>
+                <Footer/>
+            </Container>
+
         </>
     );
 }
