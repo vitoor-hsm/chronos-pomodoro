92ce5a328e296f42aa63dfae176fff2f214d54fb
Victor <vhgo2851@gmail.com>
2026-03-23 18:05:00 -0300

    criando novas paginas

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,22 @@
+import { Container } from "../../components/Container";
+import { CountDown } from "../../components/CountDown";
+import { MainForm } from "../../components/MainForm";
+import { MainTemplate } from "../../templets/MainTemplate";
+
+export function Home() {
+    return (
+        <>
+            <MainTemplate>
+                <Container>
+                    <CountDown />
+                </Container>
+
+                <Container>
+                    <MainForm />
+                </Container>
+
+            </MainTemplate>
+
+        </>
+    );
+}
